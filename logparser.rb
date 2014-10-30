5f905b31aa3f04f8f79c03aa07ead2ee6cb337cc
Tamer Shlash <mr.tamershlash@gmail.com>
2014-10-30 15:40:14 +0300

    make the script work with command line agruments

--- logparser.rb
+++ logparser.rb
@@ -1,14 +1,25 @@
-requests = {
-#	"/api/users/{user_id}/count_pending_messages" => {},
-#	"/api/users/{user_id}/get_messages" => {},
-#	"/api/users/{user_id}/get_friends_progress" => {},
-#	"/api/users/{user_id}/get_friends_score" => {},
-	"/api/users/{user_id}" => {}
-}
+require 'optparse'
+
+options = {}
+optparser = OptionParser.new do |opts|
+	opts.banner = "Usage: logparse.rb [options]"
+	opts.on('-l', '--logfile FILE', 'The logfile to analys') { |v| options[:logfile] = v }
+	opts.on('-r', '--requests FILE', 'Contains requests to analys, one per line') do |v|
+		options[:requests] = v
+	end
+end
+optparser.parse!
+
+abort optparser.help unless options[:logfile] and options[:requests]
+
+requests = {}
+File.open(options[:requests],'r') do |f|
+	f.each { |l| requests[l.gsub(/\d+/,"{user_id}").strip] = {} }
+end
 
 rgx = /(?<datetime>[0-9.:T+-]*)\sheroku.*method=(?<method>.*)\spath=(?<path>.*)\shost=(?<host>.*)\sfwd="(?<fwd>.*)"\sdyno=(?<dyno>.*)\sconnect=(?<ctime>\d+)ms\sservice=(?<stime>\d+)ms\sstatus=(?<status>\d+)\sbytes=(?<bytes>\d+)/i
 
-f = File.open("sample.log","r").each do |line|
+f = File.open(options[:logfile],"r").each do |line|
 	request = line.match(rgx)
 	path = request["path"].gsub(/\d+/,"{user_id}")
 	method = request["method"]
@@ -25,7 +36,6 @@ f = File.open("sample.log","r").each do |line|
 		end
 	end
 end
-
 f.close
 
 requests.each do |path,methods|
@@ -43,16 +53,14 @@ requests.each do |path,methods|
 	end
 end
 
-puts requests
-
-printf("%-50s%-10s%-8s%-10s%-13s%-10s%-15s","PATH","METHOD","HITS","MEAN(ms)","MEDIAN(ms)","MODE(ms)","MAX_RESP_DYNO\n\n")
+printf("%-50s%-10s%-8s%-10s%-13s%-10s%-15s\n\n",
+			 "PATH","METHOD","HITS","MEAN(ms)","MEDIAN(ms)","MODE(ms)","MAX RESP DYNO")
 
 requests.each do |path,methods|
 	methods.each do |method,stats|
 		printf("%-50s%-10s%-8i",path,method,stats[:hits])
 		printf("%-10.2f%-13.2f%-10i",stats[:mean],stats[:median],stats[:mode])
-		printf("%-10s",stats[:max_dyno].first.to_s + " with " + stats[:max_dyno].last.to_s)
+		printf("%-6s with %-5i",stats[:max_dyno].first,stats[:max_dyno].last)
 		puts
 	end
 end
-
